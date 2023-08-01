694803f906537c56ed102c59354044d5a5930fb7
Martin Faucheux <martin.faucheux@gitguardian.com>
2023-08-01 21:31:16 +0200

    scale card on hover

--- frontend/src/components/SessionCardBoard.js
+++ frontend/src/components/SessionCardBoard.js
@@ -14,7 +14,11 @@ import {
 } from "@chakra-ui/react";
 
 const SessionCard = () => (
-  <Card maxW="md">
+  <Card
+    maxW="md"
+    _hover={{ transform: "scale(1.02)" }} // Scaling effect on hover
+    transition="transform 0.3s ease" // Smooth transition on hover
+  >
     <Box>
       <Image
         src="https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1220&q=80"
