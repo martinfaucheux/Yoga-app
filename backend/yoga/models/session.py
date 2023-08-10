29219720c4e16be99110ac520506b73ce41ee2a7
Martin Faucheux <martin.faucheux@gitguardian.com>
2023-08-10 08:38:08 +0200

    add base class for models

--- backend/yoga/models/session.py
+++ backend/yoga/models/session.py
@@ -1,16 +1,14 @@
 from django.db import models
+from utils.models import BaseModel
 
 
-class Session(models.Model):
+class Session(BaseModel):
     name = models.CharField(max_length=120)
     description = models.TextField(blank=True, default="")
     picture_url = models.URLField(blank=True, null=True)
     yoga_type = models.CharField(max_length=120)
     duration = models.PositiveIntegerField()
     start_at = models.DateTimeField()
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
 
     def _str_(self):
         return self.name
