67a19e77447af1b207e4cc70851305f0ebed8d7b
Martin Faucheux <martin.faucheux@gitguardian.com>
2023-08-22 08:12:24 +0200

    set settings debug

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,14 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 ENVIRONMENT = os.environ.get("ENVIRONMENT", "dev")
 IS_DEV = ENVIRONMENT == "dev"
+DEBUG = IS_DEV
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "api"]
 
