172ec08b023bd338d852ab41d894d48e48d5569c
Martin Faucheux <martin.faucheux@gitguardian.com>
2023-08-24 18:31:02 +0200

    set host as email sender

--- backend/utils/email.py
+++ backend/utils/email.py
@@ -1,15 +1,13 @@
+from django.conf import settings
 from django.core.mail import send_mail
-from django.template.loader import render_to_string
 from django.utils.html import strip_tags
-
-SENDER_EMAIL = "martin.faucheux@gmail.com"
 
 
 def send_email(subject: str, html_message: str, addresses: list[str]):
     return send_mail(
         subject,
         strip_tags(html_message),
-        SENDER_EMAIL,
+        settings.EMAIL_HOST_USER,
         addresses,
         html_message=html_message,
         fail_silently=False,
