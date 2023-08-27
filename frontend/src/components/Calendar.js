259aa1ded7cf23377f5bd933be63e4deda80c86e
Martin Faucheux <martin.faucheux@gitguardian.com>
2023-08-27 17:14:07 +0200

    add calendar view

--- frontend/src/components/Calendar.js
+++ frontend/src/components/Calendar.js
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
-import { Box, VStack, Container, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  VStack,
+  Container,
+  Heading,
+  Text,
+  Center,
+} from "@chakra-ui/react";
 import "./Calendar.css";
 
 function CalendarView() {
@@ -8,19 +15,47 @@ function CalendarView() {
 
   return (
     <Container
-      className="app"
       maxW="lg"
       py={{ base: "12", md: "24" }}
       px={{ base: "0", sm: "8" }}
     >
       <VStack spacing={5}>
         <Heading>React Calendar</Heading>
-        <Box className="calendar-container">
-          <Calendar onChange={setDate} value={date} />
+        <Box
+          boxShadow={{ base: "none", sm: "md" }}
+          borderRadius={{ base: "none", sm: "md" }}
+        >
+          <Box
+            as={Calendar}
+            onChange={setDate}
+            value={date}
+            selectRange={false}
+            tileContent={CalendarTile}
+          />
         </Box>
         <Text>Selected Date: {date.toDateString()}</Text>
       </VStack>
     </Container>
   );
 }
+
+const CalendarTile = ({ activeStartDate, date, view }) => {
+  const isDate = date.getDay() === 2;
+  return <Circle size={2} transparent={isDate} />;
+};
+
+const Circle = ({ size, transparent }) => {
+  return (
+    <Center>
+      <Box
+        mt={2}
+        w={size}
+        h={size}
+        borderRadius="50%"
+        bg={transparent ? "emerald.500" : `rgba(0, 0 0, 0)`}
+      />
+    </Center>
+  );
+};
+
 export default CalendarView;
